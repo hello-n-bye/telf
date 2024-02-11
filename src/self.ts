6d779049ece0e3e035680ebba2e74acc1e169ba4
Logan Makris <134660747+Suno0526@users.noreply.github.com>
2024-02-11 05:48:17 -0800

    Fix silly variable mistake

--- src/self.ts
+++ src/self.ts
@@ -1,7 +1,7 @@
 import { Client, Message } from "discord.js-selfbot-v13";
 import ready from './listeners/ready';
 
-const userToken = ''
+const userToken = '';
 
 console.log('Launching Telf ...');
 
